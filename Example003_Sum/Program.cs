362299031813a7d339a02b86cbb8afbd1796cccd
Artem <kardan97@yandex.ru>
2021-10-24 17:35:00 +0700

    Random and chenge plase added

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -4,3 +4,8 @@ int numberB = new Random().Next(1,10);
 Console.Write(numberB);
 int sum = numberA + numberB;
 Console.WriteLine(" = " + sum);
+// поменять значения местами, не используя третью переменную
+numberA = numberA + numberB;
+numberB = numberA -numberB;
+numberA = numberA - numberB;
+Console.WriteLine(numberA + " <-> " + numberB);
