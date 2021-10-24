0a7a87012452fd46da9c5349fa54ffedcec6b6c2
Artem <kardan97@yandex.ru>
2021-10-24 19:30:31 +0700

    Some changes

--- Example007_magic/Program.cs
+++ Example007_magic/Program.cs
@@ -1,21 +1,21 @@
-﻿Console.Clear();                // очистка экрана консоли
+﻿Console.Clear();                
 //Console.SetCursorPosition(10,4);  // установить курсор на позицию: отступ от левого края 10 символов, отступ от верхнего края 4 символа 
-//Console.WriteLine("+");
+// //Console.WriteLine("+");
 int xa = 50, ya = 1,              // задаем координаты вершин треугольника
     xb = 1,  yb = 30,
     xc = 100, yc = 30;
 
 Console.SetCursorPosition(xa, ya);   
-Console.WriteLine("*");
+Console.WriteLine("+");
 Console.SetCursorPosition(xb, yb);   
-Console.WriteLine("*");
+Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);   
-Console.WriteLine("*");
+Console.WriteLine("+");
 
-int x = xa, y = ya;
+int x = xa, y = xb;
 int count = 0;
 
-while(count < 10000)
+while(count < 7000)
 {
     int what = new Random().Next(0,3);  // [0;3) 0 1 2
     if(what == 0)
@@ -34,6 +34,6 @@ while(count < 10000)
         y = (y + yc) / 2;
     }
     Console.SetCursorPosition(x, y);
-    Console.WriteLine("*");
+    Console.WriteLine(".");
     count++;
 }
