ec43a421773b5064c6c014768f2a87ff323f4f3d
Artem <kardan97@yandex.ru>
2021-10-30 11:47:04 +0700

    Array fill and print functions project added

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+     while (index < length)
+     {
+         collection[index] = new Random().Next(1, 10);
+         index++; 
+     }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] +" ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+     int position = -1;
+     while (index < count)
+     {
+         if (collection[index] == find)
+         {
+             position = index;
+             break;
+         }
+         index++;
+     }
+     return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+if (pos == -1)
+{
+    Console.WriteLine("Элемент 4 в массиве не найден");
+}
+else
+{
+    Console.WriteLine($"Элемент 4 находится в массиве под индексом {pos}");
+}
