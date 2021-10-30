21146bee323554ff6a5329607672361c18c8dbbc
Artem <kardan97@yandex.ru>
2021-10-30 15:55:34 +0700

    count deviders project modified

--- Example_simple_numbers/Program.cs
+++ Example_simple_numbers/Program.cs
@@ -21,7 +21,6 @@
 int Counter(int n)
 {
     int[] simpleNumber = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193 };
-    int[] array = new int[30];
     int index = 0;
     int count = 0;
     int devider = 0;
@@ -53,5 +52,5 @@ int Counter(int n)
 
 
 //int n = 32; // 1..2_147_000_000
-int result = Counter(24);
-Console.WriteLine($"result = {result}");
+int result = Counter(28);
+Console.WriteLine($"Итого различных делителей = {result}");
