fd35963679200486c63a9d54e56abdc2e82fe6f4
Artem <kardan97@yandex.ru>
2021-10-30 19:29:09 +0700

    our task

--- Ex_our_task/Program.cs
+++ Ex_our_task/Program.cs
@@ -4,7 +4,7 @@
     int index = 0;
      while (index < length)
      {
-         collection[index] = new Random().Next(10, 99);
+         collection[index] = new Random().Next(10, 100);  // рандомные числа от 10 до 99
          index++; 
      }
 }
@@ -15,7 +15,8 @@ void PrintArray(int[] col)
     int position = 0;
     while (position < count)
     {
-        Console.Write(col[position] +" ");
+        if (col[position] != 0)
+            Console.Write(col[position] +" ");
         position++;
     }
 }
@@ -37,10 +38,75 @@ void PrintArray(int[] col)
 //      return position;
 // }
 
+// void SelectArray(int[] col)
+// {
+//     int count = col.Length;
+//     int[] selected = new int[count];
+//     int index = 0;
+//     int average = 0;
+//     int sum = 0;
+//     while (index < count)
+//     {
+//         sum = sum + col[index];
+//         index++;
+//     }
+//     average = sum / count;
+//     Console.WriteLine($"Среднее равно = {average}");
+//     index = 0;
+//     while (index < count)
+//     {
+//         if (col[index] % 2 != 0) 
+//         {
+//             arrayB[index] = col[index];
+//         }   
+//         index++;
+//     }
+//  //   return selected;
+// }
+
+
+
 int[] arrayA = new int[20];
 int[] arrayB = new int[20]; 
+
+int count = arrayA.Length;
+int index = 0;
+int average = 0;
+int sum = 0;
+int max = arrayA[0];
+
 FillArray(arrayA);
 PrintArray(arrayA);
+
+while (index < count)
+{
+    sum = sum + arrayA[index];
+    index++;
+}
+average = sum / count;
+Console.WriteLine();
+Console.WriteLine($"Среднее равно = {average}");
+index = 0;
+while (index < count)
+{
+    if ( (arrayA[index] % 2 != 0) && (arrayA[index] > average) && (arrayA[index] > max) ) 
+    {
+        arrayB[index] = arrayA[index];  
+        max = arrayA[index];  
+    }
+    else 
+    {
+        arrayB[index] = 0;
+    }   
+
+    index++;
+}
+
+
+Console.WriteLine();
+//SelectArray(arrayA);
+PrintArray(arrayB);
+Console.WriteLine();
 // Console.WriteLine();
 // int pos = IndexOf(array, 4);
 // if (pos == -1)
