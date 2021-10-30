cfcdbbeb3345e57b834f1248ea50165aae10e22d
Artem <kardan97@yandex.ru>
2021-10-30 16:24:37 +0700

    count deviders project modified

--- Example_simple_numbers/Program.cs
+++ Example_simple_numbers/Program.cs
@@ -18,25 +18,31 @@
 // if(n < 1) Console.WriteLine("Число не натуральное");
 
 
-int Counter(int n)
+void Counter(int n)
 {
     int[] simpleNumber = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193 };
     int index = 0;
-    int count = 0;
+    int countTwo = 0;
+    int minimal = simpleNumber[simpleNumber.Length-1];
+    int count_diff = 0;
     int devider = 0;
     while (n > 1)
     {
         if (n % simpleNumber[index] == 0)
         {
             Console.Write(simpleNumber[index] + " ");
-            // if (simpleNumber[index] == 2)
-            // {
-            //     count++;
-            // }
+            if (simpleNumber[index] == 2)
+            {
+                countTwo++;
+            }
+            if (simpleNumber[index] < minimal)
+            {
+                minimal = simpleNumber[index];
+            }
             if(simpleNumber[index] != devider)
             {
                 devider = simpleNumber[index];
-                count++;
+                count_diff++;
             }
             n = n / simpleNumber[index];
         }
@@ -45,13 +51,16 @@ int Counter(int n)
             index++;
         }
     }
-
-    // Console.WriteLine(count);
-    return count;
+    Console.WriteLine();
+    Console.WriteLine($"Итого двоек: {countTwo}");
+    Console.WriteLine($"Минимальный множитель: {minimal}");
+    Console.WriteLine($"Итого различных делителей: {count_diff}");
+  //  return count;
 }
 
 
 //int n = 32; // 1..2_147_000_000
-int result = Counter(90);
-Console.WriteLine();
-Console.WriteLine($"Итого различных делителей = {result}");
+// int result = Counter(90);
+// Console.WriteLine();
+// Console.WriteLine($"Итого различных делителей = {result}");
+Counter(200);
