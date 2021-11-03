710ad4ace4c16bb37c20cde4a2a12edf99f6e43e
Artem <kardan97@yandex.ru>
2021-11-03 11:20:28 +0700

    Methods project added

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,76 @@
+﻿string text = "—   Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = "qwerty"
+//             012345 
+// s[3] = r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+// string newtext = Replace(text, ' ', '|');
+// Console.WriteLine(newtext);
+// Console.WriteLine();
+// newtext = Replace(text, 'к', 'К');
+// Console.WriteLine(newtext);
+// Console.WriteLine();
+// newtext = Replace(text, 'С', 'с');
+// Console.WriteLine(newtext);
+//Console.WriteLine(DateTime.Now.TimeOfDay );  // выводит информацию о системных дате и времени на экран
+
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSortForward(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+void SelectionSortBack(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[minPosition]) minPosition = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+PrintArray(arr);
+SelectionSortForward(arr);
+PrintArray(arr);
+SelectionSortBack(arr);
+PrintArray(arr);
